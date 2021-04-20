9331fd6207640265dd4ccff807eac457e7f8f4ef
Luicil Fernandes <luicilfernandes@hotmail.com>
2021-04-20 11:00:41 -0300

    INSERT

---
+++ index.js
@@ -0,0 +1,32 @@
+var database = require("./database");
+
+//#region INSERT
+
+var dadosInsert = [
+    {
+        nome: "GTA",
+        preco: 101
+    },
+    {
+        nome: "GTB",
+        preco: 50.51
+    },
+    {
+        nome: "GTC",
+        preco: 404
+    }
+];
+
+// var query = database.insert(dadosInsert).into("games");
+// console.log("==================");
+// console.log("Query:");
+// console.log(query.toQuery());
+
+database.insert(dadosInsert).into("games").then((data) =>{
+    console.log(data);
+}).catch((error) =>{
+    console.log(error);
+});
+
+//#endregion INSERT
+
